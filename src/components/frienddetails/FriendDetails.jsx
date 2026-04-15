75739abc83f4e9d01ce373af409218b0ab29a30a
rakibulhasanutsho <162662167+rakibulhasanutsho@users.noreply.github.com>
2026-04-15 18:26:55 +0600

    working about friend Details page

--- src/components/frienddetails/FriendDetails.jsx
+++ src/components/frienddetails/FriendDetails.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { RiTimerFlashLine } from "react-icons/ri";
+import { RiArchiveLine } from "react-icons/ri";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 function FriendDetails({friend}) {
     const {
@@ -8,7 +11,10 @@ function FriendDetails({friend}) {
         days_since_contact,
         status,
         bio,
-        email
+        email,
+        goal,
+        next_due_date,
+        
 
     } = friend
   return (
@@ -40,9 +46,39 @@ function FriendDetails({friend}) {
                 <p className='text-[16px  text-[#64748B]'>preferred: <span>{email}</span></p>
 
             </div>
+            <div className='my-5 space-y-3'>
+                <span className='flex gap-2 justify-center p-2 w-full shadow-sm'>
+                  <RiTimerFlashLine className='my-auto'></RiTimerFlashLine>
+                  <p className='font-semibold '>Snooze 2 Weeks</p>
+                </span>
+                <span className='flex gap-2 justify-center  p-2 w-full shadow-sm'>
+                  <RiArchiveLine className='my-auto'></RiArchiveLine>
+                  <p className='font-semibold '>Archive</p>
+                </span>
+                <span className='flex justify-center gap-2 p-2 w-full shadow-sm '>
+                  <RiDeleteBin6Line className='my-auto'></RiDeleteBin6Line>
+                  <p className='font-semibold text-red-400 '>Delete</p>
+                </span>
+                
+
+            </div>
         </div>
         <div className='flex-2'>
-
+                 <div className='flex justify-between'>
+                    <div className='p-5 px-7 shadow-sm text-center'>
+                        <h3 className='text-3xl font-bold text-[#244D3F]'>{days_since_contact}</h3>
+                        <p className='text-[#64748B]'>Days Since Contact</p>
+                    </div>
+                    <div className='p-5 shadow-sm text-center'>
+                        <h3 className='text-3xl font-bold text-[#244D3F]'>{goal}</h3>
+                        <p className="text-[#64748B]">Goal (Days)</p>
+                    </div>
+                    
+                    <div className='p-5 shadow-sm text-center'>
+                        <h3 className='text-3xl font-bold text-[#244D3F]'>{next_due_date}</h3>
+                       <p className='text-[#64748B]'> Next Due</p>
+                        </div>
+                 </div>
         </div>
     </div>
   )
